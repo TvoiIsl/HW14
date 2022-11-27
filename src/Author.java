35d95b7e79edd79f3e051e0147ef93245b20c524
Islam <islam.bedretdinov@yandex.ru>
2022-11-27 23:03:05 +0300

    ДЗ №14-2

---
+++ src/Author.java
@@ -0,0 +1,27 @@
+public class Author {
+    private String nameAuthor;
+
+    public Author(String nameAuthor) {
+        this.nameAuthor = nameAuthor;
+    }
+
+    public String getNameAuthor() {
+        return nameAuthor;
+    }
+
+    public String toString() {
+        return nameAuthor;
+    }
+
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Author author1 = (Author) other;
+        return nameAuthor.equals(author1.nameAuthor);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(nameAuthor);
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+    private String bookName;
+    private Author authorName;
+    private int publishingYear;
+
+    public Book(String bookName, Author authorName, int publisherYear) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.publishingYear = publisherYear;
+    }
+
+    public String getBookName(){
+        return bookName;
+    }
+    public Author getAuthorName(){
+        return authorName;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+    public void setPublishingYear(int publishingYear){
+        this.publishingYear=publishingYear;
+    }
+    public String toString() {
+        return bookName +authorName+ publishingYear;
+    }
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+       Book book1 = (Book) other;
+        return bookName.equals(book1.bookName);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(bookName);
+    }
+}
